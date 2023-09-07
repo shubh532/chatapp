71f71590f9bf6cab82a8bd1807aa926907127b0f
shubh532 <shubhammamahulkar2000>
2023-09-07 18:15:55 +0530

    Login functionality

--- src/Components/Authentications/SignUp.js
+++ src/Components/Authentications/SignUp.js
@@ -29,6 +29,21 @@ function SignUp() {
             alert(err.response.data.message)
         }
     }
+    async function LoginBtnHandler(e) {
+        e.preventDefault()
+        try {
+            const Response = await axios.post("http://localhost:4000/authenction/login", {
+                Email: getEmail.current.value,
+                Password: getPassword.current.value
+            })
+            if (Response.status === 200) {
+                alert(Response.data.message)
+            }
+        } catch (err) {
+            alert(err.response.data.message)
+        }
+    }
+
 
     return (
         <div className='w-10/12 lg:w-2/5 md:w-3/5 p-6 shadow-lg shadow-gray-500 rounded-lg m-auto translate-y-24'>
@@ -51,7 +66,7 @@ function SignUp() {
                     <input type="password" ref={getPassword} required className="w-full rounded-md shadow-sm focus:border-blue-200 shadow-gray-400 border border-gray-400 focus:border-2" placeholder="**********" />
                 </div>
                 <div className="flex justify-between ">
-                    <button onClick={SignUpBtnHandler} className="text-white bg-blue-500 mr-2 duration-300 focus:bg-blue-700 hover:bg-blue-700 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">{show ? "SignUp" : "Login"}</button>
+                    <button onClick={show ? SignUpBtnHandler : LoginBtnHandler} className="text-white bg-blue-500 mr-2 duration-300 focus:bg-blue-700 hover:bg-blue-700 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">{show ? "SignUp" : "Login"}</button>
                     <button onClick={FormTogglingHandler} className="text-blue-500 rounded-lg ml-2 hover:bg-blue-100 font-medium text-sm border-2 border-blue-500 duration-300 w-full sm:w-auto px-5 py-2.5 text-center">{show ? "Already Have Account" : "Don't Have Account"}</button>
                 </div>
             </form>

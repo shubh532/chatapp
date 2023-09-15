ee16c7decba284de94321802c6adbe95ee42ea86
shubh532 <shubhammamahulkar2000>
2023-09-15 23:41:04 +0530

    Some Changes to Create group Modal

--- src/Components/Chat/AddGroupUser.js
+++ src/Components/Chat/AddGroupUser.js
@@ -1,21 +1,65 @@
+import { useState } from "react";
 import Card from "../../UI Components/Card";
-import { HiMiniUserPlus } from "react-icons/hi2"
+import { HiMiniUserPlus, HiMiniCheck } from "react-icons/hi2"
+import { LiaGrinAlt, } from "react-icons/lia";
 import Avatar from "../../UI Components/Avatar";
 import { useSelector } from "react-redux/es/hooks/useSelector";
+import Picker from '@emoji-mart/react'
+
 
 function AddGroupUser() {
     const Users = useSelector(state => state.UserData.users)
+    const [message, typeMessage] = useState("")
+    const [showEmojiChart, unShowEmojiChart] = useState(false)
+
+    const onClickEmojiHandler = (e) => {
+        let sym = e.unified.split("-");
+        let codesArray = [];
+        sym.forEach((el) => codesArray.push("0x" + el));
+        let emoji = String.fromCodePoint(...codesArray);
+        typeMessage(message + emoji);
+    }
+
+    const EmojiHandler = (e) => {
+        e.preventDefault()
+        unShowEmojiChart(!showEmojiChart)
+    }
+
+    const getGroupNameHandler=(e)=>{
+        e.preventDefault()
+        typeMessage(e.target.value)
+    }
+
+    const createGroupHandler=()=>{
+        
+    }
 
     return (
         <Card>
-            <div className="w-96 flex items-center border-b-2 p-2 border-b-gray-600 rounded-md">
-                <span className="bg-slate-300 rounded-full p-3">
-                    <HiMiniUserPlus className="text-2xl" />
+            <div className="shadow-xl p-2 ">
+                <form className="flex items-center justify-between" >
+                    <div className="flex items-center">
+                        <input className="w-72 border-none focus:ring-0" maxLength={30} placeholder="Group Name" type="text" required value={message} onChange={getGroupNameHandler} ></input>
+                        <button onClick={EmojiHandler}>
+                            <LiaGrinAlt className="text-2xl" />
+                        </button>
+                    </div>
+                    <button className="bg-slate-300 p-2 rounded-full mr-2">
+                        <HiMiniCheck className="text-2xl" />
+                    </button>
+                </form>
+            </div>
+            {showEmojiChart && <div className="absolute top-14 right-0">
+                <Picker skin={5} onEmojiSelect={onClickEmojiHandler} previewPosition={"none"} />
+            </div>}
+            <div className="w-96 flex items-center p-1 mt-1 rounded-md">
+                <span className="bg-slate-300 rounded-full p-1">
+                    <HiMiniUserPlus />
                 </span>
-                <span className="ml-2 text-xl font-semibold">Add Users</span>
+                <span className="ml-2 font-semibold">Add Users</span>
             </div>
             <ul className="overflow-x-auto h-[32rem]" >
-                {Users.map((user) => 
+                {Users.map((user) =>
                     <li key={user.id} className="p-2  flex items-center">
                         <Avatar height="3" width="3" />
                         <span className="ml-2 text-xl font-semibold">{user.Name}</span>
